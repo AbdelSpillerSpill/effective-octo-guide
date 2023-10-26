d20c0a7d06e3fc708b20ec552508ea15f0c32100
AbdelSpillerSpill <110854329+AbdelSpillerSpill@users.noreply.github.com>
2023-10-26 17:06:05 +0200

    Update isLeapYear.py

--- isLeapYear.py
+++ isLeapYear.py
@@ -1,4 +1,8 @@
 def isLeapYear(year):
+    if not isinstance(year, int):
+        raise TypeError("Input must be an integer")
+    if year < 0:
+        raise ValueError("Year must be a non-negative integer")
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     else:
